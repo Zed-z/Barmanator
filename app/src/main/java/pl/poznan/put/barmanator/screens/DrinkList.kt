dd0da55ec8343151d8bcf968af4324f42de5f5a7
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-05-21 23:10:27 +0200

    Detail view fix,, better search bar

--- app/src/main/java/pl/poznan/put/barmanator/screens/DrinkList.kt
+++ app/src/main/java/pl/poznan/put/barmanator/screens/DrinkList.kt
@@ -32,10 +32,12 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -125,82 +127,12 @@ fun DrinkListScreenPreview() {
 }
 
 @Composable
-fun DrinkList(
-    drinks: List<Drink> = ArrayList<Drink>(),
-    modifier: Modifier = Modifier,
-    onDrinkClick: (Long) -> Unit,
-    filter: (Drink) -> Boolean,
+fun DrinkListScreen(drinks: List<Drink>, modifier: Modifier = Modifier, filter: (Drink) -> Boolean = { true }) {
 
-) {
     var query by remember {mutableStateOf("")}
     var dat = LocalDatabase.current
     var refreshTrigger by remember {  mutableIntStateOf(0) }
-    var actualDrinks  = remember(refreshTrigger)  {mutableStateOf(dat.getDrinks()) };
-
-
-    Column {
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = modifier.fillMaxSize(),
-            content = {
-                val drinksFiltered = actualDrinks.value.filter { filter(it) } . filter { it.name.lowercase().contains(query.lowercase()) }
-                items(drinksFiltered.size) { drinkId ->
-                    val drink = drinksFiltered[drinkId]
-                    DrinkListItem(drink = drink, onClick = { onDrinkClick(drink.id) })
-                }
-            }
-        )
-
-        Row(
-            modifier = Modifier.padding(16.dp).height(64.dp)
-        ) {
-            TextField(
-                value = query,
-                singleLine = true,
-                modifier = Modifier.weight(5f).fillMaxHeight(),
-                shape = RoundedCornerShape(32.dp),
-                placeholder = {
-                    Text(
-                        text = "Filter / Search...",
-                    )
-                },
-                onValueChange = { query = it },
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent
-                ),
-                trailingIcon = {
-                    if (query.isNotEmpty()) {
-                        Button(
-                            modifier = Modifier.weight(1f).fillMaxHeight(),
-                            onClick = {
-                                CoroutineScope(Dispatchers.IO).launch {
-                                    try {
-                                        dat.SearchDrink(query)
-                                    } catch (e: Exception) {
-                                        println("Exception searching for drink $query")
-                                    } finally {
-                                        // to się wykona zawsze, na końcu
-                                        refreshTrigger++
-                                    }
-                                }
-                            }) {
-                            Icon(
-                                modifier = Modifier.size(48.dp).fillMaxHeight(),
-                                painter = painterResource(id = R.drawable.websearch),
-                                contentDescription = "Search"
-                            )
-                        }
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun DrinkListScreen(drinks: List<Drink>, modifier: Modifier = Modifier, filter: (Drink) -> Boolean = { true }) {
+    var actualDrinks  = remember(refreshTrigger)  {mutableStateOf(dat.getDrinks()) }
 
     val configuration  = LocalConfiguration.current
     val isTablet = configuration.screenWidthDp >= 600
@@ -229,45 +161,110 @@ fun DrinkListScreen(drinks: List<Drink>, modifier: Modifier = Modifier, filter: 
         }
     }
 
-    Row(modifier.fillMaxSize()) {
-
-        DrinkList(
-            drinks,
-            onDrinkClick = { drinkId ->
-                selectedDrinkHistory = selectedDrinkHistory + drinkId
-            },
-            modifier = modifier.weight(1f),
-            filter = filter
-        )
-
-        if (isTablet) {
-            selectedDrinkId?.let { id ->
-                val drink = drinks.find { it.id == id }
-                drink?.let {
-                    DrinkDetail(
-                        isTablet = isTablet,
-                        modifier = Modifier.weight(3f),
-                        drink = it,
-                        onBack = {
-                            if (selectedDrinkHistory.size > 1) {
-                                selectedDrinkHistory = selectedDrinkHistory.dropLast(1)
+    Scaffold(
+        modifier.fillMaxSize(),
+        floatingActionButton = {
+            if (isTablet || selectedDrinkId == null) {
+                Box(
+                    modifier = Modifier
+                        .padding(start=32.dp, end=16.dp)
+                        .fillMaxWidth()
+                ) {
+                    TextField(
+                        value = query,
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth().height(64.dp),
+                        shape = RoundedCornerShape(32.dp),
+                        placeholder = {
+                            Text(
+                                text = "Filter / Search...",
+                            )
+                        },
+                        onValueChange = { query = it },
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ),
+                        trailingIcon = {
+                            if (query.isNotEmpty()) {
+                                Button(
+                                    //modifier = Modifier.weight(1f).fillMaxHeight(),
+                                    onClick = {
+                                        CoroutineScope(Dispatchers.IO).launch {
+                                            try {
+                                                dat.SearchDrink(query)
+                                            } catch (e: Exception) {
+                                                println("Exception searching for drink $query")
+                                            } finally {
+                                                // to się wykona zawsze, na końcu
+                                                refreshTrigger++
+                                            }
+                                        }
+                                    }) {
+                                    Icon(
+                                        modifier = Modifier.size(48.dp).fillMaxHeight(),
+                                        painter = painterResource(id = R.drawable.websearch),
+                                        contentDescription = "Search"
+                                    )
+                                }
                             }
                         }
                     )
                 }
             }
-        } else {
-            if (selectedDrinkId != null) {
-                val drink = drinks.find { it.id == selectedDrinkId }
-                drink?.let {
-                    DrinkDetail(
-                        isTablet = isTablet,
-                        modifier = modifier,
-                        drink = it,
-                        onBack = {
-                            selectedDrinkHistory = listOf()
-                        }
-                    )
+        }
+    ) { contentPadding ->
+
+        Row(
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = modifier.weight(1f),
+                content = {
+                    val drinksFiltered = actualDrinks.value.filter { filter(it) }
+                        .filter { it.name.lowercase().contains(query.lowercase()) }
+                    items(drinksFiltered.size) { drinkId ->
+                        val drink = drinksFiltered[drinkId]
+                        DrinkListItem(
+                            drink = drink,
+                            onClick = {
+                                selectedDrinkHistory = selectedDrinkHistory + drink.id
+                            }
+                        )
+                    }
+                }
+            )
+
+            if (isTablet) {
+                selectedDrinkId?.let { id ->
+                    val drink = drinks.find { it.id == id }
+                    drink?.let {
+                        DrinkDetail(
+                            isTablet = isTablet,
+                            modifier = Modifier.weight(3f),
+                            drink = it,
+                            onBack = {
+                                if (selectedDrinkHistory.size > 1) {
+                                    selectedDrinkHistory = selectedDrinkHistory.dropLast(1)
+                                }
+                            }
+                        )
+                    }
+                }
+            } else {
+                if (selectedDrinkId != null) {
+                    val drink = drinks.find { it.id == selectedDrinkId }
+                    drink?.let {
+                        DrinkDetail(
+                            isTablet = isTablet,
+                            modifier = modifier,
+                            drink = it,
+                            onBack = {
+                                selectedDrinkHistory = listOf()
+                            }
+                        )
+                    }
                 }
             }
         }
